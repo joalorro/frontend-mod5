b6c29e4c7a6b95a080656d0f68bc10a5efb5dc7d
joalorro <joalorro@gmail.com>
2018-11-12 21:24:13 -0500

    changed exercise cards for patients box-sizing

--- src/Routes.js
+++ src/Routes.js
@@ -2,8 +2,6 @@ import Home from './generalviews/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React, { Fragment } from 'react'
 import Login from './generalviews/Login'
-import TherapistLogin from './therapistviews/components/TherapistLogin'
-import PatientLogin from './patientviews/components/PatientLogin'
 import Signup from './generalviews/Signup'
 import PatientSignup from './patientviews/components/PatientSignup'
 import TherapistSignup from './therapistviews/components/TherapistSignup'
@@ -18,8 +16,6 @@ const Routes = () => {
 					<Route exact path="/:slug/exercises" render={(routeProps) => <ExerciseContainerForPatient {...routeProps} />} />
 					<Route exact path="/:slug/patients" render={(routeProps) => <PatientsContainer {...routeProps} />} />
 					<Route path="/login" render={(routeProps) => <Login {...routeProps} />}/>
-					<Route exact path="/login/therapist" render={(routeProps) => <TherapistLogin {...routeProps} />}/>
-					<Route exact path="/login/patient" render={(routeProps) => <PatientLogin {...routeProps} />}/>
 					<Route exact path="/signup" render={(routeProps) => <Signup {...routeProps} />} />
 					<Route exact path="/signup/patient" render={(routeProps) => <PatientSignup {...routeProps} />} />
 					<Route exact path="/signup/therapist" render={(routeProps) => <TherapistSignup {...routeProps} />} />
