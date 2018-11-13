8d5516d2619dbf0f910298736b19a749231b44da
joalorro <joalorro@gmail.com>
2018-11-13 12:22:35 -0500

    replaced flagged button with a flag icon with effects

--- src/patientviews/components/PatientExerciseCard.js
+++ src/patientviews/components/PatientExerciseCard.js
@@ -2,30 +2,60 @@ import React, { Component } from 'react';
 import PatientCommentSection from './PatientCommentSection'
 import YouTube from 'react-youtube'
 import AppAdapter from '../../adapters/AppAdapter'
+import { Icon } from 'semantic-ui-react'
+
 import '../../stylesheets/patientviews.css'
 
 class PatientExerciseCard extends Component {
 
-	state = {
-		showComments: false,
-		flagged: this.props.exercise.flagged
+	constructor(props){
+		super(props)
+		let className = this.props.exercise.flagged ? 'flagged' : ''
+		this.state = {
+			flagged: this.props.exercise.flagged,
+			className,
+			showConcernMsg: false
+		}
 	}
 
 	handleFlagToggle = () => {
 		AppAdapter.toggleFlag(this.props.exercise.id)
 		.then(response => {
-			this.setState({flagged: response})
+			let className = response ? 'flagged' : ''
+			this.setState({
+				flagged: response,
+				className
+			}, () => console.log(this.state))
 		})
 	}
 
-	displayFlagButton = () => {
-		let flagged = this.state.flagged ? "flagged" : "not-flagged"
-		let flaggedText = this.state.flagged ? 'Flagged' : 'Not Flagged'
+	handleMouseOver = () => this.setState({ showConcernMsg: true })
+
+	handleMouseOut = () => this.setState({ showConcernMsg: false })
+
+	showConcernMsg = () => {
+		let msg = this.state.flagged ? 'Concern Raised' : 'Raise Concern?'
+		return (
+			<div className='concern-msg'>
+				{msg}
+			</div>
+		)
+	}
+
+	displayConcernButton = () => {
+		let iconName = this.state.flagged ? 'flag' : 'flag outline'
 		
 		return (
-			<button onClick={this.handleFlagToggle}
-					className={"flag-btn " + flagged + " smaller"}
-				>{flaggedText}</button> 
+			<div className="flag-div">
+				{this.state.showConcernMsg ? this.showConcernMsg() : null}
+				<Icon enabled 
+					name={iconName}
+					className={this.state.className}
+					onClick={this.handleFlagToggle} 
+					onMouseOver={this.handleMouseOver}
+					onMouseOut={this.handleMouseOut}
+				/>
+			</div> 
 		)
 	}
 
@@ -39,7 +69,7 @@ class PatientExerciseCard extends Component {
 		}
 		return (
 			<div className="patient-exercise-card hvr-fade">
-				{this.displayFlagButton()}
+				{this.displayConcernButton()}
 				<h4>{this.props.exercise.name}</h4>
 				<div className="written-content">
 					<p>{this.props.exercise.desc}</p>
